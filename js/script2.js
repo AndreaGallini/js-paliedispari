e78857caffb993a780e56e9e523ea95d8e7558ab
AndreaGallini <andrea.gallini01@libero.it>
2022-10-17 09:38:02 +0200

    new

--- js/script2.js
+++ js/script2.js
@@ -3,28 +3,31 @@ let max = 5;
 let inputPoD =document.getElementById('pOd')
 let inputNumero = document.getElementById('numero')
 let btn = document.querySelector('button')
-let testo = document.querySelector('h1')
+let testo = document.getElementById('testo')
 let numerorandom = function(){
-    let numeroUtente = inputNumero.value;
+    let numeroUtente = parseInt( inputNumero.value);
     let pOd = inputPoD.value;
     console.log(numeroUtente);
 let min = 1;
 let max = 5;
     function getRndInteger(min, max) {
 
-        return Math.floor(Math.random() * (max - min) ) + min;
+        return Math.floor(Math.random() * (5 - 1) ) + 1;
 
   }
-  let somma = numeroUtente + getRndInteger(1,5)
+  console.log(getRndInteger())
+  let somma = numeroUtente + getRndInteger()
   console.log(somma , somma % 2)
     if(somma % 2 === 0 && pOd === 'pari' ){
-        testo.innerHTML = 'Hai vinto !! '
+        testo.innerHTML = `
+        Il numero uscito è ${somma}, è pari quindi  hai VINTO !!!! `
         console.log('vero')
         return true;
        
-    } else if (somma % 2 === 0 && pOd === 'dispari' ) {
-        console.log('false')
-        testo.innerHTML = 'Hai perso !! '
+    } else if (somma % 3 === 0 && pOd === 'dispari' ) {
+        console.log('dispari')
+        testo.innerHTML = `
+        Il numero uscito è ${somma}, è dispari quindi hai VINTO !!!!`
 
         return false;
     }else{
